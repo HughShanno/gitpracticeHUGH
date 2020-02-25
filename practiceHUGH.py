7fbe3c75a5deb2eab235434ae4f3cc4a9fd3504b
Hugh Shanno <hughshanno@Hughs-MacBook-Air.local>
2020-02-25 11:38:05 -0800

    Added assignment

--- practiceHUGH.py
+++ practiceHUGH.py
@@ -1,6 +1,7 @@
 #Partner 1: Hugh Shanno
 #Partner 2: Daniel Elleman
 #################################
+#Assignment Name: GitHub Practice
 def getNRandom(n):
 	'''takes in an integer and returns a list of n random integers between 1 and 10, inclusive'''
     pass
