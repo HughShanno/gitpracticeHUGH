c68e17af06dbe704e60c76238df91e218826934c
Daniel Elleman <ellemand@Daniels-MacBook-Air.local>
2020-02-25 11:39:27 -0800

    added date

--- practiceHUGH.py
+++ practiceHUGH.py
@@ -1,7 +1,7 @@
 #Partner 1: Hugh Shanno
 #Partner 2: Daniel Elleman
 #################################
-#Assignment Name: GitHub Practice
+#Assignment Name: GitHub Practice - 2/25/20
 def getNRandom(n):
 	'''takes in an integer and returns a list of n random integers between 1 and 10, inclusive'''
     pass
